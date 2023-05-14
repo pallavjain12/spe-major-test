bffbfbda5ebdc7e5e9b721ff0ebdffcc87ac7bb3
Pallav Jain <pallavjain12@gmail.com>
2023-05-14 12:29:14 +0530

    changed db url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const MongoDBStore = require("connect-mongo")(session);
 
 // const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/spe-major';
-const dbUrl = 'mongodb://localhost:27017/spe-major';
+const dbUrl = 'mongodb://mongo:27017/spe-major';
 // 'mongodb://localhost:27017/spe-major'
 // 'mongodb://mongo:27017/spe-major'
 mongoose.connect(dbUrl, {
