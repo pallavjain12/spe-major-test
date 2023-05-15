003912a0ad76e0bfbe9e9797bf03bf1749cdef3b
Pallav Jain <pallavjain12@gmail.com>
2023-05-15 15:09:51 +0530

    Added condition for db and added env variable in docker file

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ const mongoSanitize = require('express-mongo-sanitize');
 const MongoDBStore = require("connect-mongo")(session);
 
 // const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/spe-major';
-const dbUrl = 'mongodb://mongo:27017/spe-major';
+const dbUrl = 'mongodb://localhost:27017/spe-major';
+console.log(process.env.docker)
+if (process.env.docker !== undefined && process.env.docker==='true')
+    dbUrl = 'mongodb://mongo:27018/spe-major';
 // 'mongodb://localhost:27017/spe-major'
 // 'mongodb://mongo:27017/spe-major'
+console.log(dbUrl)
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
